8f55249a5dc6bc6c40bbbc3902d1b5e4eef2db34
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2023-04-15 13:01:01 +0530

    Time: 225 ms (62.74%), Space: 10.3 MB (29.56%) - LeetHub

---
+++ 0204-count-primes/0204-count-primes.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int countPrimes(int n) {
+         vector<bool> v(n+1,true);
+        v[0]=v[1]=false;
+        int count=0;
+        for(int i=2;i<n;i++){
+            if(v[i]){
+                count++;
+                for(int j=2*i;j<n;j=j+i){
+                    v[j]=false;
+                }
+            }
+        }
+        return count;
+    }
+};
