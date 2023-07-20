e851ce14277c0c9b609c4b4e74f777e433778e50
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2023-07-20 20:18:21 +0530

    Time: 10 ms (53.89%), Space: 43.4 MB (12.78%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        for(int i=0,j=s.length()-1;i<s.length()/2;i++,j--){
+            if(s.charAt(i)!=s.charAt(j)) return false;
+        }
+        return true;
+        
+    }
+}
