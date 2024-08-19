04fe3ebdcbf20c1dd453878754f4862a39bb7d44
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-08-19 23:35:47 +0530

    Time: 140 ms (11.06%), Space: 79.1 MB (13.37%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -5,7 +5,7 @@ public:
         map<int,int>mp;
         while(r<fruits.size()) {
             mp[fruits[r]]++;
-            while(mp.size()>2) {
+            if(mp.size()>2) {
                 mp[fruits[i]]--;
                 if(mp[fruits[i]]==0) {
                     mp.erase(fruits[i]);
@@ -14,8 +14,9 @@ public:
             }
             if(mp.size()<=2) {
                 maxi=max(maxi,r-i+1);
-                r++;
+                
             }
+            r++;
         }
         return maxi;
             
