093197c747348cb8b55275292fa4b834a3be67a7
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-09-01 00:20:29 +0530

    Time: 158 ms (5.15%), Space: 77.3 MB (7.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.cpp
+++ 0217-contains-duplicate/0217-contains-duplicate.cpp
@@ -3,8 +3,11 @@ public:
     bool containsDuplicate(vector<int>& nums) {
         map<int,int>mp;
         for(int i=0;i<nums.size();i++) {
-            if(mp.find(nums[i])!=mp.end()) return true;
+            
             mp[nums[i]]++;
+        }
+        for(int i=0;i<nums.size();i++) {
+            if(mp[nums[i]]>=2) return true;
         }
         return false;
     }
