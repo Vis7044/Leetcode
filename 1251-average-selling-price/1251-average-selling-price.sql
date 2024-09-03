c497b02738fb3ecca0c9aa5325e6f7f0a578b789
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-09-03 15:15:56 +0530

    Time: 805 ms (30.81%), Space: 0B (100.00%) - LeetHub

--- 1251-average-selling-price/1251-average-selling-price.sql
+++ 1251-average-selling-price/1251-average-selling-price.sql
@@ -1,6 +1,6 @@
 SELECT 
     p.product_id,
-    IFNULL(ROUND(SUM(p.price * u.units) / SUM(u.units), 2), 0) AS average_price
+     ROUND(IFNULL(SUM(p.price * u.units) / SUM(u.units), 0), 2) AS average_price
 FROM 
     Prices p
 LEFT JOIN 
