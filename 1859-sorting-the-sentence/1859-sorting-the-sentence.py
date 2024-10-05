7a2d9131358700b65da156e3a25b0fe1c766cf9f
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-10-06 01:00:32 +0530

    Time: 7 ms (93.31%), Space: 11.6 MB (94.71%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.py
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.py
@@ -1,12 +1,11 @@
-class Solution {
-    public String sortSentence(String s) {
-        String word[] = s.split(" ");
-        String result[] = new String[word.length];
+class Solution(object):
+    def sortSentence(self, s):
+        sen = s.split(" ")
+        words = [""] * 9
+    
+        for w in sen:
+            index = int(w[-1])-1
+            words[index] = w[:-1]
+        result = " ".join(word for word in words if word)
+        return result
         
-        for(String w: word) {
-            int index = w.charAt(w.length()-1) - '0'-1;
-            result[index] = w.substring(0,w.length()-1);
-        }
-        return String.join(" ",result);
-    }
-}
