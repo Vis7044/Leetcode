feac516bc7200f29040ed3c209796c11b1353bb4
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-10-17 00:42:06 +0530

    Time: 7 ms (100.00%), Space: 45 MB (97.12%) - LeetHub

--- 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
+++ 2938-separate-black-and-white-balls/2938-separate-black-and-white-balls.java
@@ -1,19 +1,14 @@
 class Solution {
     public long minimumSteps(String s) {
-        long totalSwaps = 0;
-        int blackBallCount = 0;
-
-        // Iterate through each ball (character) in the string
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '0') {
-                // Swap with all black balls to its left
-                totalSwaps += (long) blackBallCount;
-            } else {
-                // Increment the count
-                blackBallCount++;
+        long total1 = 0;
+        long ans = 0;
+        
+        for(int i=0;i<s.length();i++) {
+            if(s.charAt(i)=='1') total1++;
+            else {
+                ans += total1;
             }
         }
-
-        return totalSwaps;
+        return ans;
     }
 }
