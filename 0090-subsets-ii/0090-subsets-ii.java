7909e8eaab9865d69f91d576370eb60bdcef2baa
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-11-05 23:23:08 +0530

    Time: 1 ms (99.84%), Space: 43.2 MB (89.51%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.java
+++ 0090-subsets-ii/0090-subsets-ii.java
@@ -1,10 +1,9 @@
 class Solution {
     void solve(int ind,List<List<Integer>> ls,ArrayList<Integer> temp,int []nums) {
-        if(ls.contains(temp)) {
-            return;
-        }
+        
         ls.add(new ArrayList<>(temp));
         for(int i=ind;i<nums.length;i++) {
+            if(i>ind && nums[i-1]==nums[i]) continue;
             temp.add(nums[i]);
             solve(i+1,ls,temp,nums);
             temp.remove(temp.size()-1);
