0c148b6d5befb3756f5fd7f1b4ddff0aa47c8e80
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-11-14 14:10:07 +0530

    Time: 28 ms (82.56%), Space: 31.1 MB (42.38%) - LeetHub

---
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    bool solve(int i,int j,string p,string s, vector<vector<int>> &dp) {
+        if(i==0 && j==0) return true;
+        if(i==0 && j>0) return false;
+        if(j==0 && i>0) {
+            for(int ii=1;ii<=i;ii++) {
+                if(p[ii-1]!='*') return false;
+            }
+            return true;
+        }
+        if(dp[i][j] != -1) return dp[i][j];
+        if(p[i-1]==s[j-1] || p[i-1]=='?'){
+            return dp[i][j] = solve(i-1,j-1,p,s,dp);
+        }
+        if(p[i-1]=='*') {
+            return dp[i][j] = solve(i-1,j,p,s,dp) || solve(i,j-1,p,s,dp);
+        }
+        return dp[i][j] = false;
+    }
+    bool isMatch(string s, string p) {
+        int n = p.length(),m=s.length();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        dp[0][0] = true;
+        for(int j=1;j<=m;j++) {
+            dp[0][j] = false;
+        }
+        for(int i=1;i<=n;i++) {
+            bool flag = true;
+            for(int ii=1;ii<=i;ii++) {
+                if(p[ii-1]!='*') {
+                    flag = false;
+                    break;
+                }
+            }
+            dp[i][0] = flag;
+        }
+
+        for(int i=1;i<=n;i++) {
+            for(int j=1;j<=m;j++) {
+                if(p[i-1]==s[j-1] || p[i-1]=='?'){
+                    dp[i][j] = dp[i-1][j-1];
+                }
+                else if(p[i-1]=='*') {
+                    dp[i][j] = dp[i-1][j] || dp[i][j-1];
+                }
+                else  dp[i][j] = false;
+            }
+        }
+        return dp[n][m];
+    }
+};
