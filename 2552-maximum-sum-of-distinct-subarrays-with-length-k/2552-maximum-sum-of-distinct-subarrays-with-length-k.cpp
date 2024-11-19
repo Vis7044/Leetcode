5b47759a1775063265219293f887bc313fb5bd53
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2024-11-19 21:27:40 +0530

    Time: 123 ms (37.1%), Space: 99.6 MB (36.72%) - LeetHub

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/2552-maximum-sum-of-distinct-subarrays-with-length-k.cpp
@@ -3,21 +3,20 @@ public:
     long long maximumSubarraySum(vector<int>& nums, int k) {
         int n = nums.size();
         long long sum=0,max_sum=0;
-        map<int,int>mp;
+        unordered_set<int>st;
         int i=0,j=0;
         while(j<n) {
             sum+=nums[j];
-            mp[nums[j]]++;
-            while(mp[nums[j]]>1) {
+            while(st.count(nums[j])) {
                 sum-=nums[i];
-                mp[nums[i]]--;
+                st.erase(nums[i]);
                 i++;
             }
-
+            st.insert(nums[j]);
             if(j-i+1==k) {
                 max_sum = max(sum,max_sum);
                 sum-=nums[i];
-                mp[nums[i]]--;
+                st.erase(nums[i]);
                 i++;
             }
             j++;
