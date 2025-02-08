c8853b67fbf314c644de848d54e4254767059036
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2025-02-08 20:40:46 +0530

    Time: 0 ms (100%), Space: 43.7 MB (13.29%) - LeetHub

--- 3429-special-array-i/3429-special-array-i.java
+++ 3429-special-array-i/3429-special-array-i.java
@@ -4,9 +4,9 @@ class Solution {
             if(nums[0]%2==0 && nums[1]%2==0 ||  nums[0]%2!=0 && nums[1]%2!=0) return false;
             return true;
         }
-        for(int i=1;i<nums.length-1;i++) {
-            if((nums[i]%2==0 && nums[i-1]%2==0) ||  (nums[i]%2==0 && nums[i+1]%2==0)) return false;
-            else if((nums[i]%2!=0 && nums[i-1]%2!= 0) || (nums[i]%2!=0 && nums[i+1]%2!= 0)) return false;
+        for(int i=1;i<nums.length;i++) {
+            if((nums[i]%2==0 && nums[i-1]%2==0) ) return false;
+            else if((nums[i]%2!=0 && nums[i-1]%2!= 0) ) return false;
         }
         return true;
     }
