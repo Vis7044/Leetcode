01ab1d61e4e3c6f5ac24e8115e6ac370e381f69c
Vis7044 <99420911+Vis7044@users.noreply.github.com>
2025-02-08 20:39:21 +0530

    Time: 1 ms (33.49%), Space: 43.5 MB (43.73%) - LeetHub

---
+++ 3429-special-array-i/3429-special-array-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isArraySpecial(int[] nums) {
+        if(nums.length==2) {
+            if(nums[0]%2==0 && nums[1]%2==0 ||  nums[0]%2!=0 && nums[1]%2!=0) return false;
+            return true;
+        }
+        for(int i=1;i<nums.length-1;i++) {
+            if((nums[i]%2==0 && nums[i-1]%2==0) ||  (nums[i]%2==0 && nums[i+1]%2==0)) return false;
+            else if((nums[i]%2!=0 && nums[i-1]%2!= 0) || (nums[i]%2!=0 && nums[i+1]%2!= 0)) return false;
+        }
+        return true;
+    }
+}
