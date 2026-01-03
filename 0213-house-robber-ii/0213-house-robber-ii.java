44e1eb0956f4e41745fee2d15c8b1c10e7ee0e8c
vishal <99420911+Vis7044@users.noreply.github.com>
2026-01-03 15:11:53 +0530

    Time: 0 ms (100%), Space: 42.9 MB (38.59%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int rob(int[] nums) {
+        int  n = nums.length;
+
+        if(n==1) return nums[0];
+         int case1 = linear(nums, 0, n - 2);
+        int case2 = linear(nums, 1, n - 1);
+
+        return Math.max(case1, case2);
+    }
+
+    public int linear(int[] nums,int start, int end) {
+        int prev1=0,prev2=0;
+        for(int i=start;i<=end;i++) {
+            int curr = Math.max(nums[i]+prev2,prev1);
+            prev2 = prev1;
+            prev1=curr;
+        }
+        return prev1;
+    }
+}
